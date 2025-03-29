a96494ccd6734fbcd6345c7b2b5f4ad178d01e66
lil-fahad <73719703+lil-fahad@users.noreply.github.com>
2025-03-29 04:21:10 +0300

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,6 +1,5 @@
 import logging
 logging.basicConfig(level=logging.INFO)
-import logging
 from data.polygon_api import fetch_polygon_data
 
 def load_data(symbol="AAPL", from_date="2022-01-01", to_date="2024-12-31"):
